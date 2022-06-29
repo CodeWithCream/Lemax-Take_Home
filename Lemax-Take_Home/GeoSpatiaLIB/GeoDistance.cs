0cb581db5414cf4d8cf93b1a04d66663af195113
Andrea Knez Karačić <zelenokosazelena@gmail.com>
2022-06-29 11:04:04 +0200

    feat: geospatial lib

---
+++ Lemax-Take_Home/GeoSpatiaLIB/GeoDistance.cs
@@ -0,0 +1,62 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace Take_Home.Services.Interfaces
+{
+    /// <summary>
+    /// Operations with geo data
+    /// </summary>
+    public class GeoDistance
+    {
+        private const double MinutesInDegree = 60;
+        /// <summary>
+        /// Calculates distance between 2 coordinates
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns>Calculated distance in kilometers</returns>
+        public static double Calculate(Point point1, Point point2)
+        {
+            if (point1.Equals(point2))
+            {
+                return 0;
+            }
+            else
+            {
+                var theta = point1.X - point2.X;
+                var distance_rad = Math.Acos(
+                    Math.Sin(Deg2rad(point1.Y)) * Math.Sin(Deg2rad(point2.Y)) +
+                    Math.Cos(Deg2rad(point1.Y)) * Math.Cos(Deg2rad(point2.Y)) * Math.Cos(Deg2rad(theta)));
+                var distance_deg = Rad2deg(distance_rad);
+                var distance_miles = NauticalMileToMile(distance_deg * MinutesInDegree);
+
+                return Miles2Km(distance_miles);
+            }
+        }
+
+        /// <summary>
+        /// Converts decimal degrees to radians
+        /// </summary>
+        private static double Deg2rad(double deg)
+        {
+            return deg * Math.PI / 180.0;
+        }
+
+        /// <summary>
+        /// converts radians to decimal degrees
+        /// </summary>
+        private static double Rad2deg(double rad)
+        {
+            return rad / Math.PI * 180.0;
+        }
+
+        private static double Miles2Km(double miles)
+        {
+            return miles * 1.609344;
+        }
+
+        private static double NauticalMileToMile(double nauticalMile)
+        {
+            return nauticalMile * 1.1515;
+        }
+    }
+}
---
+++ Lemax-Take_Home/GeoSpatialLibTests/GeoDistanceTests.cs
@@ -0,0 +1,61 @@
+using NetTopologySuite.Geometries;
+using Take_Home.Services.Interfaces;
+
+namespace GeoSpatialLibTests
+{
+    /// <summary>
+    /// Calculations checked based on https://www.meridianoutpost.com/resources/etools/calculators/calculator-latitude-longitude-distance.php?
+    /// </summary>
+    [TestClass]
+    public class GeoDistanceTests
+    {
+        [TestMethod]
+        public void Get_Distance_With_Equal_Points_Returns_0()
+        {
+            var point1 = new Point(15.8779049, 45.7539264);
+            var point2 = new Point(15.8779049, 45.7539264);
+
+            var distance = GeoDistance.Calculate(point1, point2);
+
+            Assert.AreEqual(0, distance);
+        }
+               
+        [TestMethod]
+        public void Get_Distance_Returns_Distance_Between_Two_Points_On_The_Same_Hemisphere()
+        {
+
+            var point1 = new Point(15.8779049, 45.7539264);
+            var point2 = new Point(15.9287167, 45.7612322);
+            var expectedDistance = 4.02;
+
+            var distance = Math.Round(GeoDistance.Calculate(point1, point2), 2);
+
+            Assert.AreEqual(expectedDistance, distance);
+        }
+
+        [TestMethod]
+        public void Get_Distance_Returns_Distance_Between_Two_Points_On_The_Opposite_Hemisphere()
+        {
+
+            var point1 = new Point(15.8779049, 45.7539264);
+            var point2 = new Point(-15.9287167, 45.7612322);
+            var expectedDistance = 2450.97;
+
+            var distance = Math.Round(GeoDistance.Calculate(point1, point2), 2);
+
+            Assert.AreEqual(expectedDistance, distance);
+        }
+
+        [TestMethod]
+        public void Get_Distance_Opration_Is_Comutative()
+        {
+            var point1 = new Point(15.8779049, 45.7539264);
+            var point2 = new Point(15.9287167, 45.7612322);
+
+            var distance1 = GeoDistance.Calculate(point1, point2);
+            var distance2 = GeoDistance.Calculate(point2, point1);
+
+            Assert.AreEqual(distance1, distance2);
+        }
+    }
+}
